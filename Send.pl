a2841681c3731cd7d879c3228b607969c17b5ea7
Treebro <Treebro@outlook.com>
2014-02-11 22:14:42 -0600

    Working with new arduino syntax

--- Send.pl
+++ Send.pl
@@ -34,82 +34,102 @@ sub scmd {
   return $response;
 }
 
-$port->write("bogus\r");
+my $string;
+$string = &scmd("");
+$string = &scmd("1 2 8 2");
+print $string;
 
-my $response = "";
-my $timeout_tries = 50;
-
-while ($timeout_tries > 0) {
-    my ($count, $data) = $port->read(255);
-    if ($count > 0) {
-        $response .= $data;
-        last if ($data =~ /\n/);
-    }
-    $timeout_tries--;
-}
-if ($timeout_tries) {
-    print "$response\n";
-} else {
-    print "Arduino is not responding\n";
-}
-$port->write("reset board\r");
-sleep 1;
-$response = "";
-$timeout_tries = 250;
-
-while ($timeout_tries > 0) {
-    my ($count, $data) = $port->read(255);
-    if ($count > 0) {
-        $response .= $data;
-        last if ($data =~ /\n/);
-    }
-    $timeout_tries--;
-}
-if ($timeout_tries) {
-    print "$response\n";
-} else {
-    print "Arduino is not responding\n";
-}
-$port->write("set pin 4 high\r");
-$response = "";
-$timeout_tries = 250;
-
-while ($timeout_tries > 0) {
-    my ($count, $data) = $port->read(255);
-    if ($count > 0) {
-        $response .= $data;
-        last if ($data =~ /\n/);
-    }
-    $timeout_tries--;
-}
-if ($timeout_tries) {
-    print "$response\n";
-} else {
-    print "Arduino is not responding\n";
-}
-$port->write("set pin 2 high\r");
-$response = "";
-$timeout_tries = 250;
-
-while ($timeout_tries > 0) {
-    my ($count, $data) = $port->read(255);
-    if ($count > 0) {
-        $response .= $data;
-        last if ($data =~ /\n/);
-    }
-    $timeout_tries--;
-}
-if ($timeout_tries) {
-    print "$response\n";
-} else {
-    print "Arduino is not responding\n";
-}
+$string = &scmd("1 1 8 1");
+print $string;
+sleep 2;
+$string = &scmd("1 1 8 0");
+print $string;
 sleep 5;
-my $string = &scmd("set pin 2 low");
+for(my $x = 0; $x < 3; $x = $x + 1){
+$string = &scmd("1 1 8 0");
 print $string;
-$string = &scmd("set pin 4 low");
+select(undef,undef,undef,0.25);
+$string = &scmd("1 1 8 1");
 print $string;
+select(undef,undef,undef,0.25);
+}
 
+#$port->write("bogus\r");
+#
+#my $response = "";
+#my $timeout_tries = 50;
+#
+#while ($timeout_tries > 0) {
+#    my ($count, $data) = $port->read(255);
+#    if ($count > 0) {
+#        $response .= $data;
+#        last if ($data =~ /\n/);
+#    }
+#    $timeout_tries--;
+#}
+#if ($timeout_tries) {
+#    print "$response\n";
+#} else {
+#    print "Arduino is not responding\n";
+#}
+#$port->write("reset board\r");
+#sleep 1;
+#$response = "";
+#$timeout_tries = 250;
+#
+#while ($timeout_tries > 0) {
+#    my ($count, $data) = $port->read(255);
+#    if ($count > 0) {
+#        $response .= $data;
+#        last if ($data =~ /\n/);
+#    }
+#    $timeout_tries--;
+#}
+#if ($timeout_tries) {
+#    print "$response\n";
+#} else {
+#    print "Arduino is not responding\n";
+#}
+#$port->write("set pin 4 high\r");
+#$response = "";
+#$timeout_tries = 250;
+#
+#while ($timeout_tries > 0) {
+#    my ($count, $data) = $port->read(255);
+#    if ($count > 0) {
+#        $response .= $data;
+#        last if ($data =~ /\n/);
+#    }
+#    $timeout_tries--;
+#}
+#if ($timeout_tries) {
+#    print "$response\n";
+#} else {
+#    print "Arduino is not responding\n";
+#}
+#$port->write("set pin 2 high\r");
+#$response = "";
+#$timeout_tries = 250;
+#
+#while ($timeout_tries > 0) {
+#    my ($count, $data) = $port->read(255);
+#    if ($count > 0) {
+#        $response .= $data;
+#        last if ($data =~ /\n/);
+#    }
+#    $timeout_tries--;
+#}
+#if ($timeout_tries) {
+#    print "$response\n";
+#} else {
+#    print "Arduino is not responding\n";
+#}
+#sleep 5;
+#my $string = &scmd("set pin 2 low");
+#print $string;
+#$string = &scmd("set pin 4 low");
+#print $string;
+#
 #$port->parity("none");
 #$port->stopbits(1);
 #$port->dtr_active(0);
